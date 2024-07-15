a4be6eb6bbb092bbdd8f6ee644bba6960d855749
Saddamk68 <saddamk68@gmai.com>
2024-07-15 11:42:41 +0530

    program to find the longest palindrom from given string

---
+++ src/myinterviewcollection/algorithms/LongestPalindrom.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myinterviewcollection.algorithms;
+
+/**
+ *
+ * @author saddam
+ */
+public class LongestPalindrom {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // For odd-length palindromes
+            int len1 = expandAroundCenter(s, i, i);
+            // For even-length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1);
+            // Get the maximum length
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // return the length of the palindrome
+        return right - left - 1;
+    }
+
+}
